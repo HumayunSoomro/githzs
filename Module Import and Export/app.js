f718e1f92b83ebe73023cdad7229535bde131113
HumayunSoomro <122106701+HumayunSoomro@users.noreply.github.com>
2023-03-04 07:46:16 +0500

    Add files via upload

---
+++ Module Import and Export/app.js
@@ -0,0 +1,21 @@
+// import Nokia from "./mobile.js";
+// const n = new Nokia();
+// n.volumeUp();
+
+// import show from "./mobile.js";
+// show();
+
+// import a from "./mobile.js";
+// console.log(a);
+
+// import { Nokia } from "./mobile.js";
+// const n = new Nokia();
+// n.volumeup();
+
+// import { show } from "./mobile.js";
+// show();
+import * as device from "./mobile.js";
+const n = new device.Nokia();
+n.volumup();
+device.show();
+console.log(device.a);
---
+++ Module Import and Export/mobile.js
@@ -0,0 +1,33 @@
+// export default class Nokia {
+//   volumeUp() {
+//     console.log("High Volume");
+//   }
+// }
+
+// export default function show() {
+//   console.log("Humayun");
+// }
+
+// const a = 10;
+// export default a;
+
+// export class Nokia {
+//   volumeup() {
+//     console.log("sdk");
+//   }
+// }
+
+// export function show() {
+//   console.log("hbl");
+// }
+
+export class Nokia {
+  volumup() {
+    console.log("hbl");
+  }
+}
+export function show() {
+  console.log("psl");
+}
+
+export const a = 8;
